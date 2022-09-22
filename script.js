b61964352035c1e473dae9ade267e6efdcec7e26
NicoDMTX <nicolas.demontoux@hotmail.com>
2022-09-22 14:39:29 +0200

    Refactor code

--- script.js
+++ script.js
@@ -1,58 +1,75 @@
 const btncreate = document.querySelector('#valid');
 const title = document.querySelector('#title');
-const elements = document.querySelectorAll('#element')
-const content = document.querySelector('#content')
-let elementsArray = [];
+const element = document.getElementById('element');
+const content = document.querySelector('#content');
+
+
+class Note {
+    constructor(color, title, content) {
+        this.color = color;
+        this.title = title;
+        this.content = content;
+    }
+
+    createNote(color = '', title = '', content = '') {
+        const container = document.createElement("div");
+        const noteTitle = document.createElement("h1");
+        const c = document.createElement('div')
+
+        container.style.backgroundColor = color
+        noteTitle.innerText = title
+        container.appendChild(noteTitle)
+        c.innerText = content
+        container.appendChild(c)
+
+        return container;
+    }
+
+}
 
 btncreate.addEventListener('click', () => {
-    createNote();
-    createTitle();
-    noteContent();
-    setNoteColor();
-    console.log(elementsArray)
-    
+    const colorPicker = document.querySelector('#colorpicker');
+    const note = new Note();
+    note.createNote(colorPicker.value, title.value, content.value)
+    element.appendChild(note)
 })
 
-const createNote = () => {
-    const newNote = document.createElement("div");
-    const noteContainer = document.querySelector('.note-container')
 
-    noteContainer.appendChild(newNote);
-    newNote.classList.add('element')
-    newNote.id = 'element'
-    elementsArray.push(newNote)
-}
 
-const createTitle = () => {
-    const noteTitle = document.createElement("h1");
-    let titleValue = title.value;
-
-    elements.forEach(element => {
-        element.appendChild(noteTitle);
-        noteTitle.classList.add('title-note')
-        return noteTitle.innerHTML = titleValue;
-    });
+// const createNote = (color) => {
     
-}
-
-const noteContent = () => {
-    const elements = document.querySelectorAll('.element')
-    const contentParagraph = document.createElement("p");
-    const contentValue = document.createTextNode(content.value);
-
-    elements.forEach(element => {
-        element.appendChild(contentValue);
-        contentParagraph.classList.add('.note-content');
-        return contentParagraph.innerHTML = contentValue;
-    })
+//     const noteContainer = document.querySelector('.note-container')
     
-}
 
-const setNoteColor = () => {
-    const colorPicker = document.querySelector('#colorpicker')
-    const elements = document.querySelectorAll('.element')
+//     noteContainer.appendChild(newNote, color);
+//     newNote.classList.add('element')
+    
+//     newNote.id = 'element'
+//     return newNote.style.backgroundColor = color
+// }
 
-    elements.forEach(element => {
-        return element.style.backgroundColor = colorPicker.value
-    })
-}
+// const createTitle = () => {
+//     const elements = document.querySelectorAll('#element')
+    
+//     let titleValue = title.value;
+
+//     elements.forEach(element => {
+//         element.appendChild(noteTitle);
+//         noteTitle.classList.add('title-note')
+//         return noteTitle.innerHTML = titleValue;
+//     });
+    
+// }
+
+// const noteContent = () => {
+//     const elements = document.querySelectorAll('.element')
+//     const contentParagraph = document.createElement("p");
+//     const contentValue = document.createTextNode(content.value);
+
+//     elements.forEach(element => {
+//         element.appendChild(contentValue);
+//         contentParagraph.classList.add('.note-content');
+//         return contentParagraph.innerHTML = contentValue;
+//     })
+    
+// }
