3d389aa1ffabd537be03eca5afd5adcb691618da
NicoDMTX <nicolas.demontoux@hotmail.com>
2022-09-22 19:45:48 +0200

    end of the refactoring and fix bug with colors

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
-const btncreate = document.querySelector('#valid');
-const title = document.querySelector('#title');
-const element = document.getElementById('element');
-const content = document.querySelector('#content');
-
+const noteContainer = document.querySelector('.note-container')
 
 class Note {
     constructor(color, title, content) {
@@ -11,11 +7,12 @@ class Note {
         this.content = content;
     }
 
-    createNote(color = '', title = '', content = '') {
+    createNote(color = '#eeee', title = '', content = '',) {
         const container = document.createElement("div");
         const noteTitle = document.createElement("h1");
         const c = document.createElement('div')
 
+        container.classList.add('element');
         container.style.backgroundColor = color
         noteTitle.innerText = title
         container.appendChild(noteTitle)
@@ -27,49 +24,14 @@ class Note {
 
 }
 
+const btncreate = document.querySelector('#valid');
+const title = document.querySelector('#title');
+const content = document.querySelector('#content');
+const colorPicker = document.querySelector('#colorpicker');
+
 btncreate.addEventListener('click', () => {
-    const colorPicker = document.querySelector('#colorpicker');
-    const note = new Note();
-    note.createNote(colorPicker.value, title.value, content.value)
-    element.appendChild(note)
+    
+    const noteBuilder = new Note(colorPicker.value, title.value, content.value)
+    const newNote = noteBuilder.createNote(colorPicker.value, title.value, content.value)
+    noteContainer.appendChild(newNote)
 })
-
-
-
-// const createNote = (color) => {
-    
-//     const noteContainer = document.querySelector('.note-container')
-    
-
-//     noteContainer.appendChild(newNote, color);
-//     newNote.classList.add('element')
-    
-//     newNote.id = 'element'
-//     return newNote.style.backgroundColor = color
-// }
-
-// const createTitle = () => {
-//     const elements = document.querySelectorAll('#element')
-    
-//     let titleValue = title.value;
-
-//     elements.forEach(element => {
-//         element.appendChild(noteTitle);
-//         noteTitle.classList.add('title-note')
-//         return noteTitle.innerHTML = titleValue;
-//     });
-    
-// }
-
-// const noteContent = () => {
-//     const elements = document.querySelectorAll('.element')
-//     const contentParagraph = document.createElement("p");
-//     const contentValue = document.createTextNode(content.value);
-
-//     elements.forEach(element => {
-//         element.appendChild(contentValue);
-//         contentParagraph.classList.add('.note-content');
-//         return contentParagraph.innerHTML = contentValue;
-//     })
-    
-// }
