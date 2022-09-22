c8fb296effa33a1789ddac685163d2773a61f705
NicoDMTX <nicolas.demontoux@hotmail.com>
2022-09-23 01:57:04 +0200

    Adding new buttons new methods

--- script.js
+++ script.js
@@ -1,25 +1,40 @@
 const noteContainer = document.querySelector('.note-container')
 
 class Note {
-    constructor(color, title, content) {
-        this.color = color;
-        this.title = title;
-        this.content = content;
+    constructor() {
+        
     }
 
     createNote(color = '#eeee', title = '', content = '',) {
         const container = document.createElement("div");
         const noteTitle = document.createElement("h1");
-        const c = document.createElement('div')
+        const c = document.createElement('p')
+        const updateBtn = document.createElement('button')
+        const deleteBtn = document.createElement('button')
+        const btncontainer = document.createElement("div");
 
         container.classList.add('element');
         container.style.backgroundColor = color
         noteTitle.innerText = title
         container.appendChild(noteTitle)
         c.innerText = content
+        c.id = 'contentText'
         container.appendChild(c)
+        container.appendChild(btncontainer);
+
+        updateBtn.innerHTML = 'Update' 
+        updateBtn.id = 'update'
+        btncontainer.appendChild(updateBtn)
+        deleteBtn.innerHTML = 'Delete' 
+        deleteBtn.id = 'delete'
+        btncontainer.appendChild(deleteBtn)
 
         return container;
+    }
+
+    updateContent(target) {
+        const content = document.querySelector('#contentText')
+        return content;
     }
 
 }
@@ -31,7 +46,24 @@ const colorPicker = document.querySelector('#colorpicker');
 
 btncreate.addEventListener('click', () => {
     
-    const noteBuilder = new Note(colorPicker.value, title.value, content.value)
+    const noteBuilder = new Note()
     const newNote = noteBuilder.createNote(colorPicker.value, title.value, content.value)
-    noteContainer.appendChild(newNote)
+
+    if (!title.value) {
+        return alert('Entrez un titre svp')
+    } else {
+        noteContainer.appendChild(newNote)
+    }
+
+    const btnUpdate = document.querySelector('#update')
+
+    btnUpdate.addEventListener('click', () => {
+        const noteBuilder = new Note()
+        const noteUpdated = noteBuilder.updateContent()
+        console.log(noteUpdated)
+    })
+    
 })
+
+
+
